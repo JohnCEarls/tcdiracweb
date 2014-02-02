6cd22792d6ebacf0a02532807e85f713310a43b6
John C. Earls <john.c.earls@gmail.com>
2014-02-02 19:20:23 +0000

    Appears to work on master web

--- tcdiracweb/__init__.py
+++ tcdiracweb/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-import default_settings
+import tcdiracweb.default_settings
 app = Flask(__name__)
-app.config.from_object('default_settings.DevelopmentConfig')
+app.config.from_object('tcdiracweb.default_settings.DevelopmentConfig')
 app.secret_key = 'I am coming for you.'
 import tcdiracweb.views
